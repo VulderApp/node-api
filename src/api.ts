e8e098abf70588aed3e12616ca9db0c9ba4b5619
MRmlik12 <mrmlik12sup@gmail.com>
2021-07-16 09:46:08 +0200

    Clean code

--- src/api.ts
+++ src/api.ts
@@ -1,70 +1,70 @@
-import axios from "axios";
-import { School } from "./response/search/school";
-import { Search } from "./response/search/search";
-import { Branch } from "./response/branches/branch";
-import { Timetable } from "./response/timetable/timetable";
+import axios from 'axios'
+import { School } from './response/search/school'
+import { Search } from './response/search/search'
+import { Branch } from './response/branches/branch'
+import { Timetable } from './response/timetable/timetable'
 
 export class Api {
   url: string;
 
-  constructor(url: string = "https://api.vulder.xyz") {
-    this.url = url;
+  constructor (url: string = 'https://api.vulder.xyz') {
+    this.url = url
   }
 
-  async getSearchedItems(keywords: string): Promise<Array<School>> {
+  async getSearchedItems (keywords: string): Promise<Array<School>> {
     return await axios
       .request<Search>({
-        method: "get",
-        url: "/search/find",
+        method: 'get',
+        url: '/search/find',
         baseURL: this.url,
         params: {
-          input: keywords,
-        },
+          input: keywords
+        }
       })
       .then((res) => {
-        if (res.status == 200) {
-          return res.data.schools;
+        if (res.status === 200) {
+          return res.data.schools
         }
 
-        return Array<School>();
-      });
+        return [] as School[]
+      })
   }
 
-  async getBranches(url: string): Promise<Array<Branch>> {
+  async getBranches (url: string): Promise<Array<Branch>> {
     return await axios
       .request<Array<Branch>>({
-        method: "get",
-        url: "/school/branch/Current",
+        method: 'get',
+        url: '/school/branch/Current',
         baseURL: this.url,
         params: {
-          url: url,
-        },
+          url: url
+        }
       })
       .then((res) => {
-        if (res.status == 200) {
-          return res.data;
+        if (res.status === 200) {
+          return res.data
         }
 
-        return new Array<Branch>();
-      });
+        return [] as Branch[]
+      })
   }
 
-  async getTimetable(url: string): Promise<Timetable> {
+  async getTimetable (url: string): Promise<Timetable> {
     return await axios
       .request<Timetable>({
-        method: "get",
-        url: "/school/timetable/Current",
+        method: 'get',
+        url: '/school/timetable/Current',
         baseURL: this.url,
         params: {
-          url: url,
-        },
+          url: url
+        }
       })
       .then((res) => {
-        if (res.status == 200) {
-          return res.data;
+        if (res.status === 200) {
+          return res.data
         }
 
-        return {} as Timetable;
-      });
+        return {} as Timetable
+      })
   }
 }
